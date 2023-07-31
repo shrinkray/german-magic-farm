05540eb27ad7c434eea0e1090c58bff2f98f52db
amy stout <asbilly92@gmail.com>
2023-07-31 15:02:33 -0400

    feat: :construction: added a lesson section
    added a lesson json data

--- src/routes/(pages)/services/+page.svelte
+++ src/routes/(pages)/services/+page.svelte
@@ -3,6 +3,9 @@
 	import Contact from '$lib/Contact.svelte';
 	import services from '$lib/data/services.json';
 	import Hero from '$lib/Hero.svelte';
+	import lessons from '$lib/data/lessons.json';
+
+	let showThird = true;
 </script>
 
 <Hero
@@ -26,10 +29,27 @@
 			<p class="content">{content}</p>
 		{/each}
 	</div>
+	<div>
+		{#each lessons as { kindOfLesson, firstPrice, firstDescription, secondPrice, secondDescription, thirdPrice, thirdDescription }}
+			<div class="flexible-grid">
+				<h3>{kindOfLesson}</h3>
+				<div class="nodollarsign">
+					<p>${firstPrice}{firstDescription}</p>
+					<p>${secondPrice} {secondDescription}</p>
+					{#if showThird}
+						<p>${thirdPrice} {thirdDescription}</p>
+					{/if}
+				</div>
+			</div>
+
+			<p />
+		{/each}
+	</div>
+
 	<Contact />
 </div>
 <div />
-<Seo title="About | Business Frontpage" description="This is about page" type="WebPage" />
+<Seo title="Services | German Magic Farm" description="This is about page" type="WebPage" />
 
 <style>
 	/*  .grid {
@@ -41,12 +61,23 @@
     grid-column: span 8;
   } */
 
+	.flexible-grid {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5rem;
+		padding-bottom: var(--size-7);
+	}
+
 	h3 {
 		padding-bottom: 0.5em;
 		margin-bottom: 0.5em;
 		font-size: 2em;
 		font-weight: 500;
 		border-bottom: 1px solid #ddd;
+	}
+
+	div.nodollarsign > p:nth-last-child(3) {
+		display: none;
 	}
 
 	.bold {
