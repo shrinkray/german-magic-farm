155ddc19c11cefc9e4ae02541af87673329fee42
amy stout <asbilly92@gmail.com>
2023-08-02 08:57:55 -0400

    added clinic page and image

--- src/routes/(pages)/clinics/+page.svelte
+++ src/routes/(pages)/clinics/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Clinics</h1>
+<script>
+	import Hero from '$lib/Hero.svelte';
+</script>
+
+<Hero
+	herotitle="Clinics:  German Magic Farm"
+	tagline="learn, practice & have fun at a clinic"
+	--bg-image="url(clinic-collage.webp)"
+	--bg-opacity="0.5"
+/>
+
+<div class="container">
+	<h1>Clinics Offered at German Magic Farm</h1>
+
+	<p>Stay tuned for our next clinic!</p>
+</div>
+
+<style>
+	.container {
+		padding-block: 2em;
+	}
+</style>
