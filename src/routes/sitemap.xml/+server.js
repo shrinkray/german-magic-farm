fd84080bdf503b323a2d235e7ecdf624dea2da57
amy stout <asbilly92@gmail.com>
2023-08-25 10:32:31 -0400

    feat: :art: trying to fix the +server.js file for sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,5 +1,5 @@
 export const GET = async () => {
-  const markup = "<xml><h1>German Magic Farm</h1></xml>";
+  const markup = "<xml><url><loc>https://www.germanmagicfarm.com/</loc><lastmod>2023-08-25T13:59:49+00:00</lastmod><priority>1.00</priority></url><url><loc>https://www.germanmagicfarm.com/contact</loc><lastmod>2023-08-25T13:59:49+00:00</lastmod><priority>0.80</priority></url><url><loc>https://www.germanmagicfarm.com/services</loc><lastmod>2023-08-25T13:59:49+00:00</lastmod><priority>0.80</priority></url><url><loc>https://www.germanmagicfarm.com/meet-pam-bauer</loc><lastmod>2023-08-25T13:59:49+00:00</lastmod><priority>0.80</priority></url><url><loc>https://www.germanmagicfarm.com/clinics</loc><lastmod>2023-08-25T13:59:49+00:00</lastmod><priority>0.80</priority></url></xml>";
 
   return new Response(markup, {
     headers: {
