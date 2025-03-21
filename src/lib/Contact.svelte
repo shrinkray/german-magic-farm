a94a996138f311cd2812e01bd8d4ee649565a2ec
Greg Miller <hello@gregmiller.io>
2025-03-21 15:57:04 -0400

    Fix: Semantic styles
    - Update appearance of fonts

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,29 +1,27 @@
 <div class="contact">
-	<h3>Contact Pam</h3>
+	<h2>Contact Pam</h2>
 	<p><strong>German Magic Farm</strong></p>
 	<p>14082 Rightmire Rd</p>
 	<p>Dundee, Mi 48131</p>
 	<p>P: (734) 652-3775</p>
-	<p>E: <a href="mailto:pam@germanmagicfarm.com">pam@germanmagicfarm.com</a></p>
+	<p>E: <a href="mailto:pam@germanmagicfarm.com" class="bold">pam@germanmagicfarm.com</a></p>
 </div>
 
 <style>
-	/* .contact {
-    grid-column: span 4;
-  } */
-	h3 {
-		/* padding-bottom: 0.5em;
-		margin-bottom: 0.5em;
-		font-size: 2em;
-		font-weight: 500;
-		border-bottom: 1px solid #ddd; */
-	}
 	.contact > * {
-		padding: 0.5em;
+		padding-bottom: 0.5em;
+		margin-bottom: 0.5em;
 	}
+
+	h2 {
+		border-bottom: 1px solid #ddd;
+	}
+
 	a {
 		text-decoration: none;
 		color: var(--primary-color);
+		font-family: 'Lato-Bold';
+		font-weight: bold;
 	}
 	a:hover {
 		text-decoration: underline;
