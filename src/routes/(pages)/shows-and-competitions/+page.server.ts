76bb4619b80cf716354cf29af505b1f759b12b26
Greg Miller <hello@gregmiller.io>
2025-03-21 23:14:59 -0400

    Delete +page.server.ts

--- src/routes/(pages)/shows-and-competitions/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-  // TODO: Add data fetching for shows and competitions when needed
-  return {
-    // Add any data you want to pass to the page here
-  };
-}; 
