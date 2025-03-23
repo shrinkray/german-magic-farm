ab9748bcf8f001c0bbf263bc65fa3f1bcf75359a
Greg Miller <hello@gregmiller.io>
2025-03-22 22:15:58 -0400

    Refactor Contact form
    - Add svg Icon backgrounds as CSS
    - Apply different colors for light and dark

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -3,8 +3,8 @@
 	<p><strong>German Magic Farm</strong></p>
 	<p>14082 Rightmire Rd</p>
 	<p>Dundee, Mi 48131</p>
-	<p>P: (734) 652-3775</p>
-	<p>E: <a href="mailto:pam@germanmagicfarm.com" class="bold">pam@germanmagicfarm.com</a></p>
+	<p class="nowrap squeeze"><span class="phone-icon icn"></span> <a href="tel:7346523775" class="bold">(734) 652-3775</a></p>
+	<p class="nowrap squeeze"><span class="mail-icon icn"></span> <a href="mailto:pam@germanmagicfarm.com" class="bold">pam@germanmagicfarm.com</a></p>
 </div>
 
 <style>
@@ -25,5 +25,8 @@
 	}
 	a:hover {
 		text-decoration: underline;
+	}
+	.squeeze {
+		font-size: 1.1rem;
 	}
 </style>
