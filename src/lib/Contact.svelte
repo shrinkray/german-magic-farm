11f6a708626476093d71a307a437d3ecad939843
Greg Miller <hello@gregmiller.io>
2025-03-24 10:39:57 -0400

    Feat: Add icons for phone and email
    - See styles encoded for light and dark backgrounds
    - Other refactoring in styles from Prettier
    - Formatting, lintng

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,10 +1,20 @@
-<div class="contact">
-	<h2>Contact Pam</h2>
-	<p><strong>German Magic Farm</strong></p>
-	<p>14082 Rightmire Rd</p>
-	<p>Dundee, Mi 48131</p>
-	<p class="nowrap squeeze"><span class="phone-icon icn"></span> <a href="tel:7346523775" class="bold">(734) 652-3775</a></p>
-	<p class="nowrap squeeze"><span class="mail-icon icn"></span> <a href="mailto:pam@germanmagicfarm.com" class="bold">pam@germanmagicfarm.com</a></p>
+<div class="contact-container">
+	<div class="contact">
+		<h2>Contact Pam</h2>
+		<p><strong>German Magic Farm</strong></p>
+		<p>14082 Rightmire Rd</p>
+		<p>Dundee, Mi 48131</p>
+		<p class="nowrap squeeze">
+			<span class="phone-icon icn"></span> <a href="tel:7346523775" class="bold">(734) 652-3775</a>
+		</p>
+		<p class="nowrap squeeze">
+			<span class="mail-icon icn"></span>
+			<a href="mailto:pam@germanmagicfarm.com" class="bold">pam@germanmagicfarm.com</a>
+		</p>
+	</div>
+	<div class="contact-image">
+		<img class="transparent-1" src="/rider-7.svg" alt="German Magic Farm" />
+	</div>
 </div>
 
 <style>
@@ -17,16 +27,34 @@
 		border-bottom: 1px solid #ddd;
 	}
 
-	a {
-		text-decoration: none;
-		color: var(--primary-color);
-		font-family: 'Lato-Bold';
-		font-weight: bold;
-	}
-	a:hover {
-		text-decoration: underline;
-	}
 	.squeeze {
 		font-size: 1.1rem;
+	}
+	.transparent-1 {
+		opacity: 0.2;
+	}
+	.contact-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: start;
+	}
+	.contact-image {
+		display: block;
+	}
+
+	@media (width < 965px) {
+		.contact-container {
+			width: 100%;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+		}
+		.contact-image {
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
 	}
 </style>
