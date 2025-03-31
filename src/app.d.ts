dbf210385f7d9bbfe240d26195ed0216fecd4a67
Greg Miller <hello@gregmiller.io>
2025-03-31 11:54:11 -0400

    chore: Config Env vars
    - Fix exported typescript vars
    - Add example placeholder

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,9 +3,18 @@
 
 declare module 'nodemailer';
 
-namespace App {
-	// interface Error {}
-	// interface Locals {}
-	// interface PageData {}
-	// interface Platform {}
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+declare module '$env/static/private' {
+	export const SECRET_API_KEY: string;
+	export const SECRET_API_USER: string;
+	export const SECRET_API_HOST: string;
+	export const SECRET_API_RECEIVER: string;
 }
