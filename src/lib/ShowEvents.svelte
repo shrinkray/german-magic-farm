e2b309a574ef146c3881e2c0f1c38672f3df01f7
Greg Miller <hello@gregmiller.io>
2025-04-06 21:39:52 -0400

    feature: setup showEvents component
    - new component to promote types of shows
    - will lead to new sections
    - or new pages
    - component to appear on home page featuring each type

---
+++ src/lib/ShowEvents.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { ShowsData } from '$lib/types/shows';
+	import showsData from '$lib/data/shows.json';
+
+	const data = showsData as ShowsData;
+</script>
+
+<section class="section">
+	<div class="container">
+		<h2>Show Events</h2>
+		<div class="auto-grid">
+			{#each data.shows as { title, location, photo, link, excerpt, button }}
+				<div class="card flow-bottom">
+					<h3>{title}</h3>
+					<a href={link}><img class="square" src={photo} alt={title} /> </a>
+					<div class="excerpt">{excerpt}</div>
+					<a class="button" href={link}>{button}</a>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<style>
+	img {
+		margin-bottom: 2rem;
+	}
+</style>
+
